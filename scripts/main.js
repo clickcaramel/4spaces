236317fc0c62b1dd6c5103597407beeeccd10370
Ivan Kapitanov <ivorybananaa@gmail.com>
2023-08-21 09:45:23 +0300

    chore: add toggling of product list

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,29 @@ productsBtn.addEventListener('click', () => {
   productsBtn.setAttribute('aria-expanded', isOpen);
 
   if (isOpen) {
-    
+    products.style.setProperty('display', 'block');
+      const anim = products.animate([
+        { transform: 'scale(0.6)', opacity: 0 },
+        { transform: 'scale(1)', opacity: 1 },
+      ], {
+        duration: 200,
+        easing: 'cubic-bezier(0, 0.55, 0.45, 1)'
+      });
+      anim.finished.then(() => {
+        anim.cancel();
+      });
   } else {
+    const anim = products.animate([
+      { transform: 'scale(1)', opacity: 1 },
+      { transform: 'scale(0.6)', opacity: 0 },
+    ], {
+      duration: 200,
+      easing: 'cubic-bezier(0, 0.55, 0.45, 1)'
+    });
+    anim.finished.then(() => {
+      anim.cancel();
+      products.style.removeProperty('display');
+    });
   }
 
 });
